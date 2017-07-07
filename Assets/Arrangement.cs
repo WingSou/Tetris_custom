e04008b5f39d6d5448a9c175710527c6c8b99afe
WingSou <lost-color@i.softbank.jp>
2017-07-07 14:49:01 +0900

    ゲーム画面生成（仮）

---
+++ Assets/Arrangement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrangement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cube;
+
+    [SerializeField]
+    private GameObject minicube;
+
+    public static GameObject[,] cubes = new GameObject[14, 23];
+
+    public static GameObject[,] minicubes = new GameObject[2, 4];
+
+    void Start ()
+    {
+        for (int i = 0; i < 14; i++)
+        {
+            for (int r = 0; r < 23; r++)
+            {                                    //6,19 中心？
+                transform.position = new Vector3(i, r, 0);
+                cubes[i, r] = Instantiate(cube, transform.position, transform.rotation);
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int r = 0; r < 2; r++)
+            {                                    //6,19 中心？
+                transform.position = new Vector3(14 + i, 23 + r, 0);
+                minicubes[i, r] = Instantiate(cube, transform.position, transform.rotation);
+            }
+        }
+
+        //壁生成
+        for (int i = 0; i < 21; i++)
+        {
+            cubes[0, i].GetComponent<cube>().c_swich = true;
+            cubes[1, i].GetComponent<cube>().c_swich = true;
+            cubes[12, i].GetComponent<cube>().c_swich = true;
+            cubes[13, i].GetComponent<cube>().c_swich = true;
+        }
+
+        ////底生成
+        for (int i = 2; i < 12; i++)
+        {
+            cubes[i, 0].GetComponent<cube>().c_swich = true;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/cube.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    [SerializeField]
+    public Material[] _material;
+
+    //箱があるか無いか
+    public bool c_swich;
+    //ピースのタイプ
+    public int cube_type;
+    //ピースの角度
+    public int cube_rotate_type;
+    //基準点か判断
+    public bool control_cube;
+    //連鎖箱かどうか
+    public bool combo_cube;
+    //連鎖方向
+    public bool combo_rotate;
+
+    //コンボが縦か、横か
+    void Combo()
+    {
+        switch(combo_rotate)
+        {
+            case true:
+
+                GetComponent<Renderer>().material = _material[1];
+
+                break;
+
+            case false:
+
+                GetComponent<Renderer>().material = _material[2];
+
+                break;
+        }
+    }
+
+    //コンボ箱かどうか
+    void ComboDecision()
+    {
+        switch (combo_cube)
+        {
+            case true:
+
+                Combo();
+
+                break;
+
+            case false:
+
+                GetComponent<Renderer>().material = _material[0];
+
+                break;
+        }
+    }
+
+    void Awake ()
+    {
+        c_swich = false;
+        cube_type = 0;
+        cube_rotate_type = 0;
+        control_cube = false;
+        combo_cube = false;
+        combo_rotate = true;//trueは縦、falseは横
+	}
+	
+	void Update ()
+    {
+		switch(c_swich)
+        {
+            //ブロックがある
+            case true:
+
+                ComboDecision();
+
+                break;
+
+            //ブロックが無い
+            case false:
+
+                GetComponent<Renderer>().material.color = Color.white;
+                cube_type = 0;
+
+                break;
+        }
+	}
+}
