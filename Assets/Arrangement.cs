41a29b0d9c870ee2c531bb6babbbe7f45b449b6a
WingSou <lost-color@i.softbank.jp>
2017-07-09 17:18:47 +0900

    各ブロックの形状登録

--- Assets/Arrangement.cs
+++ Assets/Arrangement.cs
@@ -4,49 +4,18 @@ using UnityEngine;
 
 public class Arrangement : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject cube;
-
-    [SerializeField]
-    private GameObject minicube;
-
-    public static GameObject[,] cubes = new GameObject[14, 23];
-
-    public static GameObject[,] minicubes = new GameObject[2, 4];
-
     void Start ()
     {
-        for (int i = 0; i < 14; i++)
-        {
-            for (int r = 0; r < 23; r++)
-            {                                    //6,19 中心？
-                transform.position = new Vector3(i, r, 0);
-                cubes[i, r] = Instantiate(cube, transform.position, transform.rotation);
-            }
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            for (int r = 0; r < 2; r++)
-            {                                    //6,19 中心？
-                transform.position = new Vector3(14 + i, 23 + r, 0);
-                minicubes[i, r] = Instantiate(cube, transform.position, transform.rotation);
-            }
-        }
-
-        //壁生成
+        //壁生成(ピース登録用関数流用)
         for (int i = 0; i < 21; i++)
         {
-            cubes[0, i].GetComponent<cube>().c_swich = true;
-            cubes[1, i].GetComponent<cube>().c_swich = true;
-            cubes[12, i].GetComponent<cube>().c_swich = true;
-            cubes[13, i].GetComponent<cube>().c_swich = true;
+            GetComponent<piece_registration>().Stationery_piece(0, i, 1, i, 12, i, 13, i, true, 0);
         }
 
-        ////底生成
-        for (int i = 2; i < 12; i++)
+        //底生成(関数流用の関係上、一部再定義)
+        for (int i = 1; i < 12; i = i + 4)
         {
-            cubes[i, 0].GetComponent<cube>().c_swich = true;
+            GetComponent<piece_registration>().Stationery_piece(i, 0, i + 1, 0, i + 2, 0, i + 3, 0, true, 0);
         }
     }
 	
---
+++ Assets/piece_registration.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class piece_registration : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cube;
+
+    public static GameObject[,] cubes = new GameObject[14, 23];
+
+    public static GameObject[,] minicubes = new GameObject[4, 2];
+
+    //ピースの形
+    enum Types {I = 1, O, S, Z, J, L, T};
+
+    //ピース登録用
+    public void Stationery_piece(int x1, int y1, int x2, int y2,
+                          int x3, int y3, int x4, int y4,
+                          bool exist, int piece_type)
+    {
+        cubes[x1, y1].GetComponent<cube>().c_swich = exist;
+        cubes[x2, y2].GetComponent<cube>().c_swich = exist;
+        cubes[x3, y3].GetComponent<cube>().c_swich = exist;
+        cubes[x4, y4].GetComponent<cube>().c_swich = exist;
+
+        cubes[x1, y1].GetComponent<cube>().cube_type = piece_type;
+        cubes[x2, y2].GetComponent<cube>().cube_type = piece_type;
+        cubes[x3, y3].GetComponent<cube>().cube_type = piece_type;
+        cubes[x4, y4].GetComponent<cube>().cube_type = piece_type;
+    }
+
+    //ピースの各角度の登録
+    void RotateRegistration(int RR_x, int RR_y, Action action0, Action action90, Action action270, Action action180)
+    {
+        switch(cubes[RR_x,RR_y].GetComponent<cube>().cube_rotate_type)
+        {
+            // 0°
+            case 1:
+
+                action0();
+
+                break;
+
+            // 90°
+            case 2:
+
+                action90();
+
+                break;
+
+            // -90°
+            case 3:
+
+                action270();
+
+                break;
+
+            // 180°
+            case 4:
+
+                action180();
+
+                break;
+        }
+    }
+
+    //ピースの登録
+    void TypeRegistration(int TR_x, int TR_y, bool transfer)
+    {
+        switch(cubes[TR_x, TR_y].GetComponent<cube>().cube_type)
+        {
+            case (int)Types.I:
+
+                RotateRegistration(TR_x, TR_y,
+                    () => { Stationery_piece(TR_x - 2, TR_y, TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, transfer, 1); },
+                    () => { Stationery_piece(TR_x, TR_y + 2, TR_x, TR_y + 1, TR_x, TR_y, TR_x, TR_y - 1, transfer, 1); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x, TR_y, TR_x, TR_y - 1, TR_x, TR_y - 2, transfer, 1); },
+                    () => { Stationery_piece(TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, TR_x + 2, TR_y, transfer, 1); });
+
+                break;
+
+            case (int)Types.O:
+
+                RotateRegistration(TR_x, TR_y,
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x + 1, TR_y + 1, TR_x, TR_y, TR_x + 1, TR_y, transfer, 2); },
+                    () => { Stationery_piece(TR_x, TR_y, TR_x + 1, TR_y, TR_x, TR_y - 1, TR_x + 1, TR_y - 1, transfer, 2); },
+                    () => { Stationery_piece(TR_x - 1, TR_y + 1, TR_x, TR_y + 1, TR_x - 1, TR_y, TR_x, TR_y, transfer, 2); },
+                    () => { Stationery_piece(TR_x - 1, TR_y, TR_x, TR_y, TR_x - 1, TR_y - 1, TR_x, TR_y - 1, transfer, 2); });
+
+                break;
+
+            case (int)Types.S:
+
+                RotateRegistration(TR_x, TR_y,
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x + 1, TR_y + 1, TR_x - 1, TR_y, TR_x, TR_y, transfer, 3); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x, TR_y, TR_x + 1, TR_y, TR_x + 1, TR_y - 1, transfer, 3); },
+                    () => { Stationery_piece(TR_x - 1, TR_y + 1, TR_x - 1, TR_y, TR_x, TR_y, TR_x, TR_y - 1, transfer, 3); },
+                    () => { Stationery_piece(TR_x + 1, TR_y, TR_x, TR_y, TR_x, TR_y - 1, TR_x - 1, TR_y - 1, transfer, 3); });
+
+                break;
+
+            case (int)Types.Z:
+
+                RotateRegistration(TR_x, TR_y,
+                    () => { Stationery_piece(TR_x - 1, TR_y + 1, TR_x, TR_y + 1, TR_x, TR_y, TR_x + 1, TR_y, transfer, 4); },
+                    () => { Stationery_piece(TR_x + 1, TR_y + 1, TR_x + 1, TR_y, TR_x, TR_y, TR_x, TR_y - 1, transfer, 4); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x, TR_y, TR_x - 1, TR_y, TR_x - 1, TR_y - 1, transfer, 4); },
+                    () => { Stationery_piece(TR_x - 1, TR_y, TR_x, TR_y, TR_x, TR_y - 1, TR_x + 1, TR_y - 1, transfer, 4); });
+
+                break;
+
+            case (int)Types.J:
+
+                RotateRegistration(TR_x, TR_y,
+                    () => { Stationery_piece(TR_x - 1, TR_y + 1, TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, transfer, 5); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x + 1, TR_y + 1, TR_x, TR_y, TR_x, TR_y - 1, transfer, 5); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x, TR_y, TR_x - 1, TR_y - 1, TR_x, TR_y - 1, transfer, 5); },
+                    () => { Stationery_piece(TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, TR_x + 1, TR_y - 1, transfer, 5); });
+
+                break;
+
+            case (int)Types.L:
+
+                RotateRegistration(TR_x, TR_y,
+                    () => { Stationery_piece(TR_x + 1, TR_y + 1, TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, transfer, 6); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x, TR_y, TR_x, TR_y - 1, TR_x + 1, TR_y - 1, transfer, 6); },
+                    () => { Stationery_piece(TR_x - 1, TR_y + 1, TR_x, TR_y + 1, TR_x, TR_y, TR_x, TR_y - 1, transfer, 6); },
+                    () => { Stationery_piece(TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, TR_x - 1, TR_y - 1, transfer, 6); });
+
+                break;
+
+            case (int)Types.T:
+
+                RotateRegistration(TR_x, TR_y,
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, transfer, 7); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x, TR_y, TR_x + 1, TR_y, TR_x, TR_y - 1, transfer, 7); },
+                    () => { Stationery_piece(TR_x, TR_y + 1, TR_x - 1, TR_y, TR_x, TR_y, TR_x, TR_y - 1, transfer, 7); },
+                    () => { Stationery_piece(TR_x - 1, TR_y, TR_x, TR_y, TR_x + 1, TR_y, TR_x, TR_y - 1, transfer, 7); });
+
+                break;
+        }
+    }
+
+    //ピース呼び出し用
+    void BlockType(int BT_x, int BT_y, bool rewrite)
+    {
+        if(cubes[BT_x, BT_y].GetComponent<cube>().control_cube == true)
+        {
+            TypeRegistration(BT_x, BT_y, rewrite);
+        }
+    }
+
+
+    void Awake ()
+    {
+        for (int i = 0; i < 14; i++)
+        {
+            for (int r = 0; r < 23; r++)
+            {                                    //6,19 中心？
+                transform.position = new Vector3(i, r, 0);
+                cubes[i, r] = Instantiate(cube, transform.position, transform.rotation);
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int r = 0; r < 2; r++)
+            {                                    //6,19 中心？
+                transform.position = new Vector3(14 + i, 23 + r, 0);
+                minicubes[i, r] = Instantiate(cube, transform.position, transform.rotation);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
